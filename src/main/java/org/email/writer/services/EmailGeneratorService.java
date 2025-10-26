9c13785b3c3f11c41f6244e8a40820c298a04877
Priyanshu Singh <as3585954@gmail.com>
2025-10-26 19:46:05 +0530

    Add response and get response

--- src/main/java/org/email/writer/services/EmailGeneratorService.java
+++ src/main/java/org/email/writer/services/EmailGeneratorService.java
@@ -1,9 +1,13 @@
 package org.email.writer.services;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.cdimascio.dotenv.Dotenv;
+
 import org.email.writer.models.EmailRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,21 +15,24 @@ import java.util.Map;
 @Service
 public class EmailGeneratorService {
 
+    private final WebClient webClient;
 
     private final String geminiApiUrl;
     private final String geminiApiKey;
 
-    public EmailGeneratorService() {
-        Dotenv dotenv = Dotenv.load(); // Loads .env file from root
+    public EmailGeneratorService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+        Dotenv dotenv = Dotenv.load();
         this.geminiApiUrl = dotenv.get("GEMINI_API_URL");
         this.geminiApiKey = dotenv.get("GEMINI_API_KEY");
     }
+
 
     public String generateEmailReply(EmailRequest emailRequest){
         // Build the prompt
         String prompt = buildPrompt(emailRequest);
         // Craft a Request
-        Map<String, Object> createRequest = Map.of(
+        Map<String, Object> requestBody = Map.of(
                 "contents" , new Object[]{
                         Map.of("parts" , new Object[]{
                                 Map.of("text" , prompt)
@@ -33,7 +40,31 @@ public class EmailGeneratorService {
                 }
         );
         // Do the request and get response
+        String response =  webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type","application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
         // Return the reponse
+        return extractResponseContent(response);
+    }
+
+    private String extractResponseContent(String response) {
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        }catch(Exception e){
+            return "Error Processing Request: " + e.getMessage();
+        }
     }
 
     private String buildPrompt(EmailRequest emailRequest) {
